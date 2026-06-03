8d59e5f130325910db98dff83d40a3b72d125d5e
zenmakek <devdhawan81@gmail.com>
2026-06-03 19:29:36 +0530

    refactor: improve relay server startup logging and error handling

--- server/main.go
+++ server/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/zenmakek/parcel/server/relay"
+)
 
 func main() {
-	fmt.Println("Parcel relay server starting...")
+	fmt.Println("[parcel] starting relay server...")
+
+	s := relay.New()
+	if err := s.Start(); err != nil {
+		fmt.Printf("[parcel] fatal %v\n", err)
+		os.Exit(1)
+	}
 }
