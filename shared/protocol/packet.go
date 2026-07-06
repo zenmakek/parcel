ffdd63e69af99fb95942252f53bdf3db96568158
zenmakek <devdhawan81@gmail.com>
2026-07-06 08:32:52 +0530

    phase/19: chunk transfer protocol, announce/lookup/bitfield/chunk packets

--- shared/protocol/packet.go
+++ shared/protocol/packet.go
@@ -11,4 +11,15 @@ const (
 	PacketOTPExpired       = "OTP_EXPIRED"
 	PacketOTPJoin          = "OTP_JOIN"
 	PacketOTPInvalid       = "OTP_INVALID"
+
+	PacketAnnounce         = "ANNOUNCE"
+	PacketLookup           = "LOOKUP"
+	PacketPeerList         = "PEER_LIST"
+	PacketManifestRequest  = "MANIFEST_REQUEST"
+	PacketManifestResponse = "MANIFEST_RESPONSE"
+	PacketChunkRequest     = "CHUNK_REQUEST"
+	PacketChunkResponse    = "CHUNK_RESPONSE"
+	PacketChunkVerifyFail  = "CHUNK_VERIFY_FAIL"
+	PacketHave             = "HAVE"
+	PacketBitfield         = "BITFIELD"
 )
